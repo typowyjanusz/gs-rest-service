636d24c10ee7ca476d284bf1a5afe53a2689b101
typowyjanusz <exens00@gmail.com>
2015-10-15 21:05:55 +0200

    just do it

---
+++ src/main/java/hello/Greeting.java
@@ -0,0 +1,24 @@
+package hello;
+
+/**
+ * Created by Janusz on 2015-10-15.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
