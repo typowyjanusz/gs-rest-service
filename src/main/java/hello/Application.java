1e1b0141bc078b03df294a31b78cd1426201fba3
typowyjanusz <exens00@gmail.com>
2015-10-16 17:18:00 +0200

    poprawka tomcat

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,13 +2,20 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 /**
  * Created by Janusz on 2015-10-15.
  */
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
